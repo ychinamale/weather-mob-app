342e48abd9741be4a3fd74da8e0bc6fdeb931dee
Yamikani Chinamale <ychinamale@gmail.com>
2022-02-14 23:12:24 +0200

    Added scripts & set up RN config

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {
   SafeAreaView, StatusBar, Text, View,
 } from 'react-native';
+import CONFIG from 'react-native-config';
 
 function App() {
   return (
     <SafeAreaView>
       <StatusBar />
       <View>
-        <Text>Weather app</Text>
+        <Text>Weather app with the url as: {CONFIG.API_URL}</Text>
       </View>
     </SafeAreaView>
   );
