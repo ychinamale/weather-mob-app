00c6bf536a0bce4408b6c14122b3968d9c715015
Yamikani Chinamale <ychinamale@gmail.com>
2022-02-15 00:19:24 +0200

    WIP getting current location

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
   SafeAreaView, StatusBar, Text, View,
 } from 'react-native';
 import CONFIG from 'react-native-config';
+import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
+import Geolocation from 'react-native-geolocation-service';
 
 function App() {
+
+  useFocusEffect(
+    useCallback(() => {
+      // get the current location here
+      // implement pull to refresh later
+    }, [])
+  );
+
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <View>
-        <Text>Weather app with the url as: {CONFIG.API_URL}</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <StatusBar />
+        <View>
+          <Text>Weather app with the url as: {CONFIG.API_URL}</Text>
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
