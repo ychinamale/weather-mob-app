969ae8da9aa01df3594802afd50ab28d1d11f71a
Yamikani Chinamale <ychinamale@gmail.com>
2022-02-19 17:36:47 +0200

    Added forest images and icons

---
+++ src/constants/images.tsx
@@ -0,0 +1,11 @@
+export const backgrounds = {
+  sunny: require('src/assets/images/forest_sunny.png'),
+  cloudy: require('src/assets/images/forest_cloudy.png'),
+  rainy: require('src/assets/images/forest_rainy.png'),
+}
+
+export const icons = {
+  sunny: require('src/assets/images/icons/clear.png'),
+  cloudy: require('src/assets/images/icons/partlysunny.png'),
+  rainy: require('src/assets/images/icons/rain.png'),
+}
